df83bbe408cf3e728b260b8e216bfef81667c64e
eksa-arifa <eksaarifa@gmail.com>
2024-10-30 22:00:58 +0700

    Try to fix: detail error on production

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function getDataAnime() {
 }
 
 async function getDetailAnime(anime) {
-  let { data } = await axios.get(`https://otakudesu.cloud/terbaru`);
+  let { data } = await axios.get(`https://otakudesu.cloud/anime/${anime}/`);
 
   // const $ = cheerio.load(data);
 
